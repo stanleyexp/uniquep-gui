6c1a551e8aced852dae16819b0f158eaea1fcf57
tingwei628 <sbvq6p199xe@gmail.com>
2022-03-27 03:11:53 +0800

    wip

--- main.py
+++ main.py
@@ -1,243 +1,128 @@
 from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QPushButton,
-    QMainWindow,
-    QLabel,
-    QLineEdit,
-    QMenu,
-    QAction,
-    QCheckBox,
-    QComboBox,
-    QDoubleSpinBox,
+    # QVBoxLayout,
+    # QHBoxLayout,
+    # QPushButton,
+    # QMainWindow,
+    # QLabel,
+    # QLineEdit,
+    # QMenu,
+    # QAction,
+    # QCheckBox,
+    # QComboBox,
+    # QDoubleSpinBox,
     QProgressBar,
-    QRadioButton,
-    QSpinBox,
-    QGridLayout,
-    QStackedLayout,
-    QTabWidget,
-    QFileDialog
+    # QRadioButton,
+    # QSpinBox,
+    # QGridLayout,
+    # QStackedLayout,
+    # QTabWidget,
+    QFileDialog,
+    QHeaderView
 )
-
-from MainWindow5 import Ui_Form
-
+from os.path import exists
+import sqlite3
+from UniquepUI import Ui_Form
 import sys
-class Search(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        self.btn = QPushButton("Press me")
-        layout.addWidget(self.btn)
-        # self.setStyleSheet("background-color:green;")
         
-    
-class Upload(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        self.btn = QPushButton("Presss me 2")
-        layout.addWidget(self.btn)
-        # self.setStyleSheet("background-color:red;")
-        
-    
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        
-        self.setWindowTitle("uniquep")
-        self.main_layout = QStackedLayout()
-        
-
-
-        self.label = QLabel("OK I am Label")
-        font = self.label.font()
-        font.setPointSize(20)
-        self.label.setFont(font)
-        # Qt.AlignHCenter
-        # Qt.AlignJustify
-        self.label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-
-        self.input = QLineEdit()
-        # self.input.setMaxLength(10)
-        self.input.setPlaceholderText("Enter your text")
-        self.input.returnPressed.connect(self.return_pressed)
-        # self.input.textChanged.connect(self.label.setText)
-        # self.input.textChanged.connect(self.text_changed)
-        self.input.textEdited.connect(self.text_edited)
-
-
-        # QGridLayout
-        # QGridLayout.addWidget(QWidget)
-        # QGridLayout.addWidget(QWidget, int row, int column, Qt.Alignment alignment=0)
-        # QGridLayout.addWidget(QWidget, int row, int column, int rowSpan, int columnSpan, Qt.Alignment alignment=0)
-        self.button_is_checked = True
-        self.button = QPushButton("press me")
-        self.button.setCheckable(True)
-        self.button.clicked.connect(self.the_button_was_clicked)
-        self.button.clicked.connect(self.the_button_was_toggled)
-        self.button.setChecked(self.button_is_checked)
-        inside_layout1 = QHBoxLayout()
-        # inside_layout1.setContentsMargins(0, 0, 0, 0)
-        # inside_layout1.setSpacing(20)
-        inside_layout2 = QHBoxLayout()
-        layout = QVBoxLayout()
-        inside_layout1.addWidget(self.input)
-        inside_layout2.addWidget(self.label)
-        # layout.addWidget(self.input)
-        # layout.addWidget(self.label)
-        checkBox = QCheckBox()
-        layout.addWidget(checkBox)
-        checkBox.setCheckState(Qt.Checked) # Qt.Unchecked
-        checkBox.stateChanged.connect(self.checkbox_state)
-        self.combox = QComboBox()
-        layout.addWidget(self.combox)
-        self.combox.addItems(["Option1", "Option2", "Option3"])
-        # Sends the current index (position) of the selected item.
-        self.combox.currentIndexChanged.connect(self.combox_index_state)
-        # There is an alternate signal to send the text.
-        self.combox.currentTextChanged.connect(self.combox_text_state)
-        widgets = [
-            # QCheckBox,
-            # QComboBox,
-            # QDateEdit,
-            # QDateTimeEdit,
-            # QDial,
-            # QDoubleSpinBox,
-            # QFontComboBox,
-            # QLCDNumber,
-            # QLabel,
-            # QLineEdit,
-            QProgressBar,
-            # QPushButton,
-            # QRadioButton,
-            # QSlider,
-            # QSpinBox,
-            # QTimeEdit,
-        ]
-        for w in widgets:
-            layout.addWidget(w())
-
-        
-        self.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.on_context_menu)
-        layout.addLayout(inside_layout1)
-        layout.addLayout(inside_layout2)
-        container1 = QWidget()
-        container1.setLayout(layout)
-        self.main_layout.addWidget(container1)
-        # Set the central widget of the Window.
-        self.setCentralWidget(container1)
-        # self.setCentralWidget(self.main_layout)
-
-        # self.setFixedSize(QSize(400, 300))
-        # self.setMaximumSize(QSize(400, 300))
-        # self.setCentralWidget(self.button)
-    
-    # def mouseMoveEvent(self, e):
-    #     self.label.setText("mouseMoveEvent")
-
-    # mouse right click to call the menu (contextMenuEvent, original event handler)
-    # def contextMenuEvent(self, e):
-    #     context = QMenu(self)
-    #     context.addAction(QAction("test 1", self))
-    #     context.addAction(QAction("test 2", self))
-    #     context.addAction(QAction("test 3", self))
-    #     context.exec(e.globalPos())
-    def return_pressed(self):
-        print("Return pressed!")
-        self.input.setText("BOOM!")
-
-    # def text_changed(self, s):
-    #     print("Text changed...")
-    #     print(s)
-    def text_edited(self, s):
-        print("Text edited...")
-        print(s)
-    def combox_index_state(self, s):
-        print(s)
-        # print(str(self.combox.currentText())) # get option text
-    def combox_text_state(self, s):
-        print("current option text", s)
-    def checkbox_state(self, s):
-        pass
-        # print(s == Qt.Checked)
-        # print(s)
-    def on_context_menu(self, pos):
-        context = QMenu(self)
-        context.addAction(QAction("test 1", self))
-        context.addAction(QAction("test 2", self))
-        context.addAction(QAction("test 3", self))
-        context.exec(self.mapToGlobal(pos))
-
-    def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        # disabled
-        self.button.setEnabled(False)
-        print("Clicked!")
-    def the_button_was_toggled(self, checked):
-        self.button_is_checked = checked
-        # print("Checked?", checked)
-        print(self.button_is_checked)
-
-class MainWindow2(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("test")
-        self.resize(400, 500)
-        self.btn1 = QPushButton("Search")
-        self.btn2 = QPushButton("Upload")
-        wid1 = Search()
-        wid2 = Upload()
-        container = QWidget()
-        self.main_layout = QStackedLayout()
-        container.setLayout(self.main_layout)
-        self.main_layout.addWidget(wid1)
-        self.main_layout.addWidget(wid2)
-
-        layout = QVBoxLayout()
-        layout.addWidget(container)
-        layout.addWidget(self.btn1)
-        layout.addWidget(self.btn2)
-        self.setLayout(layout)
-
-        self.btn1.clicked.connect(self.btn1_clicked)
-        self.btn2.clicked.connect(self.btn2_clicked)
-        
-    def btn1_clicked(self):
-        self.main_layout.setCurrentIndex(0)
-    def btn2_clicked(self):
-        self.main_layout.setCurrentIndex(1)
-
-class MainWindow3(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("test2")
-        self.resize(300, 200)
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        tabs = QTabWidget()
-        wid1 = Search()
-        wid2 = Upload()
-        tabs.addTab(wid1, "search")
-        tabs.addTab(wid2, "upload")
-        layout.addWidget(tabs)
-
-class MainWindow5(QWidget, Ui_Form):
+class MainWindow(QWidget, Ui_Form):
     def __init__(self, *args, obj=None, **kwargs):
         super(QWidget, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self.select_file_btn.clicked.connect(self.openFile)
+        self.search_btn.clicked.connect(self.search)
+        self.download_btn.clicked.connect(self.download)
+        self.select_file_btn.clicked.connect(self.open_file)
         self.upload_btn.clicked.connect(self.upload)
-        
+        self.prev_btn.clicked.connect(self.prev_page)
+        self.next_btn.clicked.connect(self.next_page)
+        self.jump_btn.clicked.connect(self.jump_page)
+        self.load_data()
+        self.total_page = None
+        self.total_count = None
+        self.count_per_page = 10
+        self.curr_page = 1
+    def jump_page(self):
+        pass
+    def prev_page(self):
+        self.curr_page = self.curr_page - 1
+        pass
+    def next_page(self):
+        pass
+    def uppate_page_status(self):
+        self.page_label.setText(f'curr:{str(self.curr_page)} total{str(self.total_page)}')
+        if self.curr_page < 2:
+            self.prev_btn.setEnabled(False)
+        else:
+            self.prev_btn.setEnabled(True)
+
+        if self.curr_page > self.total_page:
+            self.next_btn.setEnabled(False)
+        else:
+            self.next_btn.setEnabled(True)
+
+    def table_view_setup(self):
+        header = self.table_view.horizontalHeader()     
+        # column 1
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        # column 2
+        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        # column 3
+        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
     
+    def search(self):
+        print("query db")
+    def download(self):
+        print("download search result")
+    def connect_db(self):
+        if not exists("uniquep.db"):
+            # init db
+            connection = sqlite3.connect("uniquep.db")
+            cursor = connection.cursor()
+            """
+            column
+            url, descr, income
+            """
+
+            cursor.execute("""
+                CREATE TABLE uniquep_table
+                (url TEXT, descr TEXT, income INTEGER)
+            """)
+        
+            cursor.execute("""INSERT INTO uniquep_table VALUES 
+                ('giraffes.io', 'Uber, but with giraffes', 1900),
+                ('dronesweaters.com', 'Clothes for cold drones', 3000),
+                ('hummingpro.io', 'Online humming courses', 120000)
+            """)
+            connection.commit()
+            # print("File projects.db does not exist. Please run initdb.py.")
+            # sys.exit()
+        self.conn = QSqlDatabase.addDatabase("QSQLITE")
+        self.conn.setDatabaseName("uniquep.db")
+        if not self.conn.open():
+            sys.exit(1)
+        
+    def disconnect_db(self):
+        if not self.conn.isOpen():
+            sys.exit(1)
+        self.conn.close()
+        QSqlDatabase.removeDatabase(QSqlDatabase.database().connectionName())
+
+    def load_data(self):
+        self.connect_db()
+        self.table_model = QSqlQueryModel()
+        # self.table_model.setTable("uniquep_table")
+        # self.table_model.select()
+        self.table_model.setQuery("select * from uniquep_table")
+        self.table_view.setModel(self.table_model)
+        self.table_view_setup()
+        # self.table_view.show()
+
     # from: https://stackoverflow.com/a/44076057
-    def openFile(self):   
+    def open_file(self):   
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
@@ -245,21 +130,28 @@ class MainWindow5(QWidget, Ui_Form):
         # if fileName:
         #     print(fileName)
     def upload(self):
-        # get input
-        # get current select file
-        pass
+        # upload is a background job
+        # use QProgressBar to show progress
+        print(QProgressBar)
+        # get input (check csv format)
+        print(self.upload_input.toPlainText())
+        # get current select file (check file(.csv) format and check csv format)
+        print(self.upload_file_label.text())
+        # parsing with uniquep
+        # insert into db
+        
+def main():
+    # only one QApplication instance per application
+    # app = QApplication([])
+    app = QApplication(sys.argv)
 
-# only one QApplication instance per application
-# app = QApplication([])
-app = QApplication(sys.argv)
-# window = MainWindow()
-# window = MainWindow2()
-# window = MainWindow3()
+    # window = uic.loadUi("uniquep.ui")
+    window = MainWindow()
+    # Windows are hidden by default
+    window.show()
 
-# window = uic.loadUi("uniquep.ui")
-window = MainWindow5()
-# Windows are hidden by default
-window.show()
+    # start event loop
+    app.exec()
 
-# start event loop
-app.exec()
+if __name__ == "__main__":
+    main()
